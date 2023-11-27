c2a498c6261f7525c345f3acf0f412d3ed35d985
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-11-27 00:25:56 -0500

    first commit

---
+++ animated-nav/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Sidebar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ animated-nav/src/Components/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <ul>
+        <li>
+            <a href='/'>Home</a>
+        </li>
+        <li>
+            <a href='/'>Messages</a>
+        </li>
+        <li>
+            <a href='/'>About</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
